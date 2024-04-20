fd278b690eac60efccf8d9a71aa7866cf6f73d14
Mihhail Matvejev <m4tveevm@icloud.com>
2024-04-20 15:36:07 +0300

    small change

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import requests
 
-lat, lng = "59.971374", "30.324618"
-params = {
-    'lat': lat,
-    'lng': lng,
-    'radius': 5,
-    'categories': '',
-    'fields': '',
-    'expand': '',
-}
-r = requests.get(
-    f'https://spb-afisha.gate.petersburg.ru/kg/external/afisha/events',
-    auth=('user', 'pass'), params=params)
-print(r.text)
+
+def get_events_around(lat='59.971374', lng=30.324618, radius='5', categories=''):
+    params = {
+        'lat': lat,
+        'lng': lng,
+        'radius': 5,
+        'categories': categories,
+        'fields': '',
+        'expand': '',
+    }
+    r = requests.get(
+        f'https://spb-afisha.gate.petersburg.ru/kg/external/afisha/events',
+        auth=('user', 'pass'), params=params)
+    return r.text
+
+
+print(get_events_around())
