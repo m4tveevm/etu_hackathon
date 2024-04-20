dec9abaea74a126a22902ea8d1439d75476c04b7
Mihhail Matvejev <m4tveevm@icloud.com>
2024-04-20 15:58:34 +0300

    add dot env and requirements.txt

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import os
+
 import requests
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MAPS_API_KEY = os.environ.get("YANDEX_API")
+
 
 
 def get_events_around(lat='59.971374', lng=30.324618, radius='5', categories=''):
