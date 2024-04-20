5cca897f6a3c6a0ec20006db6b089c2312ce2a48
Name <email@example.ru>
2024-04-20 16:20:08 +0300

    new atr

--- main.py
+++ main.py
@@ -9,17 +9,21 @@ load_dotenv()
 MAPS_API_KEY = os.environ.get("YANDEX_API")
 
 
-def get_events_around_new(lat='59.971374', lng=30.324618, radius='5', categories=''):
+def get_events_around_new(lat='59.971374', lng=30.324618, radius='5', categories='', tags='', actual_since='',
+                          is_free=True):
     base_url = "https://spb-afisha.gate.petersburg.ru/kg/external/afisha/events"
     params = {
         'lat': lat,
         'lng': lng,
         'radius': 5,
-        'categories': 'exhibition,-concert',
-        'fields': 'place,title,images,dates',
+        'categories': '',
+        'fields': 'place,tags,categories,is_free,',
         'expand': 'place,images,dates',
         'page': 1,
-        'count': 10
+        'count': 10,
+        'tags': tags,
+        'actual_since': actual_since,
+        'is_free': is_free,
     }
     response = requests.get(base_url, params=params)
     if response.status_code == 200:
