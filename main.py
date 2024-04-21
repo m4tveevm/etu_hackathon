7ad22b3a0f1bb216dd0f4b17d6454f6d7a7c3ae8
Name <email@example.ru>
2024-04-21 05:47:26 +0300

    like working version?

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 import os
-
 import requests
 
-from dotenv import load_dotenv
 
-load_dotenv()
-
-MAPS_API_KEY = os.environ.get("YANDEX_API")
-
-
-def get_events_around_new(lat='59.971374', lng=30.324618, radius='5', categories='', tags='', actual_since='',
+def get_events_around_new(lat='59.971374', lng=30.324618, radius='5', categories='',tags='',
                           is_free=True):
     base_url = "https://spb-afisha.gate.petersburg.ru/kg/external/afisha/events"
     params = {
@@ -17,12 +10,11 @@ def get_events_around_new(lat='59.971374', lng=30.324618, radius='5', categories
         'lng': lng,
         'radius': 5,
         'categories': '',
-        'fields': 'place,tags,categories,is_free,',
+        'fields': 'place,tags,categories,is_free,title',
         'expand': 'place,images,dates',
         'page': 1,
         'count': 10,
         'tags': tags,
-        'actual_since': actual_since,
         'is_free': is_free,
     }
     response = requests.get(base_url, params=params)
@@ -31,6 +23,7 @@ def get_events_around_new(lat='59.971374', lng=30.324618, radius='5', categories
         events = []
         for event in data.get('data', []):
             event_info = {
+                'categories' : event['categories'],
                 'title': event['title'],
                 'place': event['place']['title'] if event.get('place') else "Не указано",
                 'image_url': event['images'][0]['image'] if event.get(
@@ -38,7 +31,7 @@ def get_events_around_new(lat='59.971374', lng=30.324618, radius='5', categories
                 'dates': event['dates'][0]['start'] if event.get('dates') else "Время не указано"
             }
             events.append(event_info)
-        return events
+        return events #Хотели чтобы тег выводило???
     else:
         return f"Ошибка запроса: {response.status_code}"
 
